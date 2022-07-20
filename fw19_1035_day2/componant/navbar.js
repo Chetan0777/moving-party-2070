2fbe653c66f85691c109048817e7f64418835dbd
Praful-87 <103850217+Praful-87@users.noreply.github.com>
2022-07-20 21:11:35 +0530

    fw19_1035_day2

---
+++ fw19_1035_day2/componant/navbar.js
@@ -0,0 +1,17 @@
+function navbar() {
+  return ` <div id="image_box">
+  <img src="https://10qnr7pp4618p5tu1sihfwn1-wpengine.netdna-ssl.com/wp-content/uploads/2022/02/MT-Logo@2x-1.png"
+      alt="">
+</div>
+<div id="links_box">
+  <p><a href="">Sales Readiness Platform</a></p>
+  <p><a href="">Solutions</a></p>
+  <p><a href="">Customers</a></p>
+  <p><a href="">Resources</a></p>
+  <p><a href="">Company</a></p>
+  <p id="request_demo"><a href="">Request Demo</a></p>
+  <p><a href="">Supriya</a></p>
+
+</div>`;
+}
+export default navbar;
---
+++ fw19_1035_day2/index.js
@@ -0,0 +1,2 @@
+import navbar from "./componant/navbar.js";
+document.getElementById("navbar").innerHTML = navbar();
