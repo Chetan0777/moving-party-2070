8e20f851aea07f4b3361a69610db3ce99889c611
Praful-87 <103850217+Praful-87@users.noreply.github.com>
2022-07-21 18:00:31 +0530

    fw19_1035_day3

--- fw19_1035/componant/navbar.js
+++ fw19_1035/componant/navbar.js
@@ -1,17 +1,135 @@
 function navbar() {
-  return ` <div id="image_box">
+  return ` 
+  <div id="image_box">
   <img src="https://10qnr7pp4618p5tu1sihfwn1-wpengine.netdna-ssl.com/wp-content/uploads/2022/02/MT-Logo@2x-1.png"
       alt="">
 </div>
 <div id="links_box">
-  <p><a href="">Sales Readiness Platform</a></p>
-  <p><a href="">Solutions</a></p>
-  <p><a href="">Customers</a></p>
-  <p><a href="">Resources</a></p>
-  <p><a href="">Company</a></p>
+  <a class="arr" href="">Sales Readiness Platform</a>
+  <a class="arr" href="">Solutions</a>
+  <a class="arr" href="">Customers</a>
+  <a class="arr" href="">Resources</a>
+  <a class="arr" href="">Company</a>
   <p id="request_demo"><a href="">Request Demo</a></p>
-  <p><a href="">Supriya</a></p>
+  <p><a href="">Register</a></p>
 
+</div>
+  `;
+}
+function footer() {
+  return `<div id="blue_bar">
+  <p>We’re proud to be recognized as an industry leader, view our <span> full list of honors</span> to learn
+      more.</p>
+  <img src="https://10qnr7pp4618p5tu1sihfwn1-wpengine.netdna-ssl.com/wp-content/uploads/2022/03/homepag-footer-banner.png"
+      alt="" style="width: 50%;">
+</div>
+<div id="f_main">
+  <div id="f_left"><img
+          src="https://10qnr7pp4618p5tu1sihfwn1-wpengine.netdna-ssl.com/wp-content/uploads/2021/11/footerLogo-noIcon-300x47.png"
+          alt="">
+      <p id="intro_text">Get free access to the best sales-readiness content.</p>
+      <div id="place_div">
+          <input type="text" placeholder="email@company.com">
+          <button>Subscribe</button>
+      </div>
+      <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
+      <label for="vehicle1">
+          I agree to Mindtickle using my contact details for marketing purposes.</label>
+      <p>By submitting this form, you agree to the <span> Mindtickle Privacy Policy.</span></p>
+  </div>
+  <div id="f_right">
+
+      <div class="sum_menu1"><a href="">Sales Readiness Platform</a>
+
+          <ul>
+              <li><a href=""> Sales Enablement & Training</a></li>
+              <li><a href=""> Sales Content Management</a></li>
+              <li><a href=""> Conversation Intelligence</a></li>
+              <li><a href=""> Sales Readiness Index</a></li>
+              <li><a href=""> Analytics & Dashboards</a></li>
+              <li><a href=""> Sales Coaching</a></li>
+              <li><a href=""> Integrations</a></li>
+              <li><a href=""> Services</a></li>
+          </ul>
+
+      </div>
+      <div class="sum_menu2"><a href="">Solutins</a>
+          <ul>
+              <li> <a href=""> Sales Onboarding</a></li>
+              <li> <a href=""> Continuous Learning</a></li>
+              <li> <a href=""> Manager-Led Coaching</a></li>
+              <li> <a href=""> Account Strategy & Review</a></li>
+              <li> <a href=""> Sales Kickoffs</a></li>
+              <li> <a href=""> Partner Enablement</a></li>
+              <li> <a href=""> Sales Certification</a></li>
+              <li> <a href=""> Voice of the Customer</a></li>
+              <li> <a href=""> For Sales Enablement Pros</a></li>
+              <li> <a href=""> For Sales Leadership</a></li>
+              <li> <a href=""> Frontline Sales Managers</a></li>
+              <li> <a href=""> Sales Operations Managers</a></li>
+          </ul>
+
+      </div>
+      <div class="sum_menu3">
+          <a href="">Customers</a>
+          <a href="">Resources</a>
+          <ul>
+              <li><a href=""> Be Ready Blog</a></li>
+              <li><a href=""> Events</a></li>
+          </ul>
+
+
+          <a href="">Company</a>
+          <ul>
+              <li><a href=""> Be Ready Blog</a></li>
+              <li><a href=""> Events</a></li>
+              <li><a href=""> Culture</a></li>
+              <li><a href=""> Careers</a></li>
+              <li><a href=""> News</a></li>
+              <li><a href=""> Contact Us</a></li>
+          </ul>
+          <a href="">Request a Demo</a>
+      </div>
+  </div>
+</div>
+<hr>
+<div id="last">
+  <div>
+      <p><span>&#169;</span> 2022 Mindtickle inc.All right reserved</p>
+      <a href="">Privacy</a>
+      <a href="">Terms of Service</a>
+      <a href="">Trust</a>
+      <a href="">Sitemamp</a>
+  </div>
+  <div><i class="fa-brands fa-linkedin-in"></i>
+      <i class="fa-brands fa-facebook-f"></i>
+
+
+  </div>
 </div>`;
 }
-export default navbar;
+function window() {
+  return `<div>
+  <h1>About Mindtckle</h1>
+  <p>Meet Mindtickle. We’re a leading sales enablement company that thrives on creating impact for our
+      customers.</p>
+  <button>Who We Are</button>
+</div>
+<div>
+  <p class="on_headings">Culture</p>
+  <p>Create sales excellence by perterning an incredble team</p>
+  <p class="on_headings">Careers</p>
+  <p>Explore job operning around globe with Mindticle</p>
+</div>
+<div>
+  <p class="on_headings">Culture</p>
+  <p>Create sales excellence by perterning an incredble team</p>
+  <p class="on_headings">Careers</p>
+  <p>Explore job operning around globe with Mindticle</p>
+</div>
+<div>
+  <p class="Contact">Contact Us</p>
+  <p>Have a question? connect with our team and get answers</p>
+</div>`;
+}
+export { navbar, footer, window };
--- fw19_1035/index.js
+++ fw19_1035/index.js
@@ -1,2 +1,19 @@
-import navbar from "./componant/navbar.js";
+import { navbar, footer, window } from "./componant/navbar.js";
 document.getElementById("navbar").innerHTML = navbar();
+document.getElementById("footer").innerHTML = footer();
+
+let arr = document.querySelectorAll(".arr");
+arr.forEach((el) => {
+  el.addEventListener("mouseover", function () {
+    add();
+  });
+  el.addEventListener("mouseout", function () {
+    del();
+  });
+});
+function add() {
+  document.querySelector("#window").innerHTML = window();
+}
+function del() {
+  document.querySelector("#window").innerHTML = null;
+}
