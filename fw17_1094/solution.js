1778ff901fd8b03b8582a47d0a88ba8880f12e0d
supriyakaa <pathaksupriya981@gmail.com>
2022-07-22 22:13:06 +0530

    fw17_1094_day4

---
+++ fw17_1094/solution.js
@@ -0,0 +1,17 @@
+import {
+    navbar,
+    footer,
+    company,
+    resourse,
+    solutions,
+    salesReadinesPlatform,
+    main,
+  } 
+  from "../fw19_1035/componant/navbar.js"
+  
+  document.getElementById("navbar").innerHTML = navbar();
+  document.getElementById("footer").innerHTML = footer();
+  let arr = document.querySelectorAll(".arr");
+  
+  // main function to
+  main(arr);
